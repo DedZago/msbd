e317c49fb77367fa6c53ef950d261256033ed1ba
lugaltaross <luisa.galtarossa@gmail.com>
2019-05-08 15:31:58 +0200

    iperparametri

--- funzioni/__init__.py
+++ funzioni/__init__.py
@@ -1,3 +1,4 @@
 from .indici import indice_gini
 from .indici import tasso_errata_classificazione
-
+from .iperparametri import grafico_metrica_iperparametro
+from .iperparametri import grafico_metrica_iperparametri
